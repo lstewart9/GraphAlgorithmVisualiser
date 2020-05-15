b1289c0619a9559cadf63301272c0f149b4b28ee
Lachlan Stewart <lstewart@lstewart-mbp.home>
2020-05-15 19:30:35 +1000

    added fabric.js API and connector lines

--- js/main.js
+++ js/main.js
@@ -1,18 +1,45 @@
 var graph
+var canvas
+var verticiesCoords = []
+var verticies = []
+
 
 $(document).ready(function() {
+	canvas = new fabric.Canvas('myCanvas');
 
 	
+	canvas.on('mouse:over', function(e) {
+		if (e.target) {
+			if(e.target._objects) {
+				// is circle
+				e = e.target._objects[0]
+				e.set('fill', 'lightgrey');
+			} 	
+			canvas.renderAll();
+		}
+	});
 
+	canvas.on('mouse:out', function(e) {
+		if (e.target) {
+			if(e.target._objects) {
+				// is circle
+				e = e.target._objects[0]
+				e.set('fill', 'lightslategray');
+			} 	
+			canvas.renderAll();
+		}
+	});
 })
+
+function randomNumber(max, min) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
 
 function generateGraph() {
 	console.log("Generating Graph...");
 
 	// generate random number of verticies
-	var max = 15;
-	var min = 2;
-	var nVerticies = Math.floor(Math.random() * (max - min + 1) + min);
+	var nVerticies = randomNumber(2,10)
 	graph = new Graph(nVerticies);
 	console.log(nVerticies)
 
@@ -25,6 +52,7 @@ function generateGraph() {
 
 }
 
+
 class Graph {
 
 	constructor(nVertices) {
@@ -32,8 +60,11 @@ class Graph {
 		this.adjMatrix = [];
 		for (var i=0; i < nVertices; i++) {
 			this.adjMatrix[i] = [];
-			for (var j=0; j < nVertices; j++) {
-				this.adjMatrix[i][j] = 0;
+		}
+		for (var i=0; i < nVertices; i++) {
+			for (var j=0; j <= i; j++) {
+				this.adjMatrix[i][j] = randomNumber(0,2);
+				this.adjMatrix[j][i] = this.adjMatrix[i][j]
 			}
 		}
 	}
@@ -55,12 +86,28 @@ class Graph {
 		//this.AdjList.get(w).push(v); 
 	} 
 
+	DFS(start, visited) {
+		visited[start] = true;
+		console.log("======")
+		for (var i = 0; i < this.nVertices; i++) {
+			if (this.adjMatrix[start][i] == 1 && visited[i] != true) {
+				verticies[i].set('fill', 'lightgreen');
+				console.log(i)
+				console.log(verticies[i])
+				setTimeout(() => { verticies[i].set('fill', 'lightslategray'); }, 2000);
+				setTimeout(() => { this.DFS(i, visited); }, 4000);
+			} 
+		}
+
+	}
+
 	printGraph() { 
 		var print = " "
 
 		for (var j=0; j < this.nVertices; j++) {
 			print = print + '   ' + j
 		}
+		print = print + ' <-j'
 		print = print + '\n'
 		for (var j=0; j < this.nVertices+2; j++) {
 			print = print + '---'
@@ -75,27 +122,129 @@ class Graph {
 		}
 		console.log(print)
 
-		var c = document.getElementById("myCanvas")
-		var ctx = c.getContext("2d")
-		var x = 50
-		var y = 50
-		ctx.font = "30px Verdana";
+		var x = 30
+		var y = 30
 		for (var i=0; i < this.nVertices; i++) {
-			ctx.beginPath()
-			ctx.arc(x, y, 40, 0, 2 * Math.PI);
-			ctx.stroke();
-			if (i < 10) {
-				ctx.fillText(i, x-10, y+10);
-			} else {
-				ctx.fillText(i, x-20, y+10);
+			var circle = new fabric.Circle({
+				originX: 'center',
+  				originY: 'center',
+				radius : 60,
+				fill: 'lightslategray',
+				hasBorders : true,
+				borderColor: "black",
+				selectionBackgroundColor :"blue"
+
+			});
+			
+			verticies.push(circle)
+
+			var text = new fabric.Text("v" + i.toString(), {
+				fontSize: 30,
+				originX: 'center',
+  				originY: 'center'
+			});
+
+			if ( i%2 == 1 ) {
+				y+=100
 			}
-			x+=150
+
+			var group = new fabric.Group([ circle, text ], {
+			left: x,
+			top: y,
+			});
+
+			canvas.add(group);
+			
+			var vertex = {
+				x: x,
+				y: y
+			}
+			verticiesCoords.push(vertex)
+
+			if ( i%2 == 1 ) {
+				y-=100
+			}
+
+			x+=200
 			if (x > 1000) {
-				x = 50
-				y+=125
+				x = 30
+				y+=300
+			}
+		}
+	//console.log(verticies);
+	this.printLinks()
+	} 
+	  
+	printLinks() {
+		
+		for (var i=0; i < this.nVertices; i++) {
+			for (var j=0; j <= i; j++) {
+				if (this.adjMatrix[i][j] == 1 && i!=j) {
+					createLine([verticiesCoords[i].x+60, verticiesCoords[i].y+60, verticiesCoords[j].x+60, verticiesCoords[j].y+60])
+				}
 			}
 		}
 
+	}
+}
+
+function createLine(points) {
+	var line = new fabric.Line(points, {
+	  strokeWidth: 5,
+	  stroke: '#7db9e8',
+	  originX: 'center',
+	  originY: 'center',
+	  hasControls: false,
+	  hasBorders: false,
+	  hasRotatingPoint: false,
+	  hoverCursor: 'default',
+	  selectable: false
+	});
+	/*
+	var headLength = 15,
+  
+		x1 = points[0],
+		y1 = points[1],
+		x2 = points[2],
+		y2 = points[3],
+  
+		dx = x2 - x1,
+		dy = y2 - y1,
+  
+		angle = Math.atan2(dy, dx);
+  
+	angle *= 180 / Math.PI;
+	angle += 90;
 	
-	} 
+	
+	var triangle = new fabric.Triangle({
+	  angle: angle,
+	  fill: '#207cca',
+	  top: y2,
+	  left: x2,
+	  height: headLength,
+	  width: headLength,
+	  originX: 'center',
+	  originY: 'center',
+	  selectable: false
+	});
+	*/
+	canvas.add(line);
+	line.sendToBack()
+	canvas.renderAll();
+}
+
+function wait(ms)
+{
+    var d = new Date();
+    var d2 = null;
+    do { d2 = new Date(); }
+    while(d2-d < ms);
+}
+
+function beginDFS() {
+	var form = $('#startVertexForm')[0].elements
+	start = parseInt(form[0].value)
+	console.log(start)
+	graph.DFS(start, [])
 }
